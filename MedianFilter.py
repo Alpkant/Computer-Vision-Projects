860d8b42d89ba9b31922c066ca15e577e107631c
Alpkant <kantarcia@itu.edu.tr>
2018-11-03 11:38:29 +0300

    MedianFilter added

---
+++ MedianFilter.py
@@ -0,0 +1,32 @@
+# Author:
+# Alperen Kantarcı
+# 150140140
+
+
+import numpy as np
+import cv2
+from PyQt5 import QtGui
+class MedianFilter(object):
+
+	def __init__(self,image,filter_size):
+		#self.image = image
+		self.I = image
+		self.filter_size = filter_size
+		self.newI = np.zeros(self.I.shape)
+		self.pixMap = None
+
+	def apply_filter(self):
+		# Add appropriate padding size to the image
+		pad_s = self.filter_size//2
+		self.I = np.pad(self.I,( (pad_s,pad_s),(pad_s,pad_s),(0,0) ),mode='constant')
+		row , column , channel = self.I.shape
+		for i in range(channel):
+			for r in range(pad_s,row-pad_s):
+				for c in range(pad_s,column-pad_s):
+					self.newI[r-pad_s,c-pad_s,i] = np.uint8(np.median(self.I[r-pad_s:r+pad_s,c-pad_s:c+pad_s,i]))
+
+		row,column,channel = self.newI.shape
+		qImg = QtGui.QImage(np.uint8(self.newI).copy(), column, row,3*column, QtGui.QImage.Format_RGB888).rgbSwapped()
+		self.pixMap = QtGui.QPixmap.fromImage(qImg)
+		return self.newI,self.pixMap
+
